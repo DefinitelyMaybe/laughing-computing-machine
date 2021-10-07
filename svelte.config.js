e70864a0c41ab9e61ecb5632b4519c7ccac0be56
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-10-07 14:26:21 +1300

    sveltekit init

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from "svelte-adapter-deno";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: adapter()
+	}
+};
+
+export default config;
